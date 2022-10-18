531e7fb6ec7a54657c707fd595db37504e23795f
juanpimartinuceda <s1970704@ed.ac.uk>
2022-10-18 10:50:21 +0100

    Managed to provide the jarName as an input and do the report just once, querying just the information needed after each graphQL call

--- src/main/java/com/shethap/tech/graphql/dao/DynamicAnalyzer.java
+++ src/main/java/com/shethap/tech/graphql/dao/DynamicAnalyzer.java
@@ -4,16 +4,29 @@ import com.shethap.tech.graphql.model.JarFile;
 import com.shethap.tech.graphql.model.Report;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public class DynamicAnalyzer {
 
-    private Report report;
+    private ArrayList<Report> reports;
 
     public DynamicAnalyzer() {
-        this.report = new Report(JarFile.getByName("ilp"));
+        this.reports = new ArrayList<>();
     }
 
-    public Report getReport(String jarName) {
-        return this.report;
+    public Report getReport(String jarName) { //The report is done just once, then returns what you ask for in the query.
+        while (true) {
+        for (Report r : this.reports) {
+            if (r.getJarFile().getJarName().equals(jarName)) {
+                return r;
+            }
+        }
+        addReport(jarName);
+        }
+    }
+
+    public void addReport(String jarName) {
+        this.reports.add(new Report(JarFile.getByName(jarName)));
     }
 }
